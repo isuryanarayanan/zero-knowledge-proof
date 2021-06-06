3fa4c45f90e828dd12207eafa3a23d120c2e6543
isuryanarayanan <devdoodan@gmail.com>
2021-06-06 23:42:58 +0530

    problem study

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h5 class="text-center">
+      Alice needs to show bob that she knows the discrete logarithm of a given
+      value with parameters know to each other
+    </h5>
+    <div class="row">
+      <div class="p-1 col-sm">
+        <div class="card bg-dark p-5">
+          <h1 class="display-4 text-light">Alice</h1>
+        </div>
+      </div>
+      <div class="p-1 col-sm">
+        <div class="card bg-dark p-5">
+          <h1 class="display-4 text-light">Bob</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
